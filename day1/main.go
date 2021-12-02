c3f3eb4917c40b69b5ef3f9cd7e096ecc4b14c71
Christian Sanders <wolph@thewhitewolph.com>
2021-12-01 20:25:46 -0600

    Day 1

---
+++ day1/main.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	// Part 1
+	var sum, prev int
+	for scanner.Scan() {
+		cur := scanner.Text()
+		curInt, err := strconv.Atoi(cur)
+		if err != nil {
+			panic(err)
+		}
+		if prev == 0 {
+			prev = curInt
+			continue
+		}
+
+		if curInt > prev {
+			sum++
+		}
+		prev = curInt
+	}
+
+	println(sum)
+
+	// Part 2
+	file2, err := os.Open("example.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file2.Close()
+
+	sum = 0
+	prev = 0
+	var tmp, tmp2, prev2, prev3 int
+	flop := true
+	scanner2 := bufio.NewScanner(file2)
+	for scanner2.Scan() {
+		cur := scanner2.Text()
+		curInt, err := strconv.Atoi(cur)
+		if err != nil {
+			panic(err)
+		}
+		if prev == 0 {
+			prev = curInt
+			continue
+		}
+		if prev2 == 0 {
+			prev2 = curInt
+			continue
+		}
+		if prev3 == 0 {
+			prev3 = curInt
+			continue
+		}
+
+		if flop {
+			tmp = prev + prev2 + prev3
+			flop = false
+		}
+
+		prev = prev2
+		prev2 = prev3
+		prev3 = curInt
+		tmp2 = prev + prev2 + prev3
+
+		if tmp2 > tmp {
+			sum++
+		}
+		tmp = tmp2
+	}
+
+	println(sum)
+}
